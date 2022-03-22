c6636d4880a8184afa15a23f03c205414b1cfd13
davide <davide.magagni@gmail.com>
2022-03-22 19:37:47 +0000

    rest removed

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 import json
 from rest_framework import viewsets
-from rest_framework.decorators import api_view
-from django.http import HttpResponse
+#from rest_framework.decorators import api_view
+#from django.http import HttpResponse
 from django.db.models import Avg
 
 from .models import Professor
@@ -9,52 +9,56 @@ from .models import Module
 from .models import ProfRating
 
 
-@api_view(['GET'])
+# @api_view(['GET'])
 def List(request):
-    subjects = Module.objects.all()
-    list = []
-    for sub in subjects:
-        professors = sub.professors.all()
-        x = ''
-        for c in professors:
-            x = x + str(c.name) + " "
-        p = {'code': sub.code, 'name': sub.name, 'professors': x}
-        list.append(p)
+    if request.method == 'GET':
+        subjects = Module.objects.all()
+        list = []
+        for sub in subjects:
+            professors = sub.professors.all()
+            x = ''
+            for c in professors:
+                x = x + str(c.name) + " "
+            p = {'code': sub.code, 'name': sub.name, 'professors': x}
+            list.append(p)
 
-    result = {"subject list": list}
-    response = HttpResponse(json.dumps(result))
-    return HttpResponse(response)
+        result = {"subject list": list}
+        response = HttpResponse(json.dumps(result))
+        return HttpResponse(response)
 
 
-@api_view(['GET'])
+# @api_view(['GET'])
 def View(request):
-    professors = Professor.objects.all()
-    list = []
-    for prof in professors:
-        p = {
-            'name': prof.name, 'average': prof.ratings.aggregate(Avg('rating'))['rating__avg']
-        }
-        list.append(p)
+    if request.method == 'GET':
+        professors = Professor.objects.all()
+        list = []
+        for prof in professors:
+            p = {
+                'name': prof.name, 'average': prof.ratings.aggregate(Avg('rating'))['rating__avg']
+            }
+            list.append(p)
 
-    result = {"Professors list": list}
-    response = HttpResponse(json.dumps(result))
-    return HttpResponse(list)
+        result = {"Professors list": list}
+        response = HttpResponse(json.dumps(result))
+        return HttpResponse(list)
 
 
-@api_view(['GET'])
+# @api_view(['GET'])
 def Average(request, id, moduleId):
-    rating = ProfRating.objects.filter(
-        professor=id, module=moduleId).aggregate(Avg('rating'))['rating__avg']
-    profName = Professor.objects.get(profId=id)
-    moduleName = Module.objects.get(code=moduleId)
-    
-    return HttpResponse(rating)
+    if request.method == 'GET':
+        rating = ProfRating.objects.filter(
+            professor=id, module=moduleId).aggregate(Avg('rating'))['rating__avg']
+        profName = Professor.objects.get(profId=id)
+        moduleName = Module.objects.get(code=moduleId)
 
-@api_view(['GET'])
+        return HttpResponse(rating)
+
+
+# @api_view(['GET'])
 def Rate(request, rate, id, moduleId, year, semester):
+    if request.method == 'GET':
+        prof = Professor.objects.get(profId=id)
+        mod = Module.objects.get(code=moduleId)
+        ProfRating.objects.create(professor=prof, module=mod, rating=rate)
 
-    prof = Professor.objects.get(profId=id)
-    mod = Module.objects.get(code=moduleId)
-    ProfRating.objects.create(professor=prof, module=mod, rating=rate)
-
-    return HttpResponse("success")
+        return HttpResponse("success")
