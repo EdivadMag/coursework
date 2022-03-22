117448900e9db134c0c343f24ac96612e58dcae1
davide <davide.magagni@gmail.com>
2022-03-22 19:45:49 +0000

    bug fix

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 import json
 from rest_framework import viewsets
 #from rest_framework.decorators import api_view
-#from django.http import HttpResponse
+from django.http import HttpResponse
 from django.db.models import Avg
 
 from .models import Professor
